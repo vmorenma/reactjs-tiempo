0829c6c276759b46652434ccdd9cb088188c4924
vmorenma <victor.moreno.maya@gmail.com>
2017-05-04 20:58:37 +0200

    first commit

---
+++ app/api/openWeatherMap.jsx
@@ -0,0 +1,21 @@
+var axios = require('axios');
+
+//api key 0eab8bafb2b3cf92a40d639572fc2f66
+
+const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?appid=0eab8bafb2b3cf92a40d639572fc2f66&units=metric';
+
+module.exports={
+  getTemp:function(location){
+    var encodedLocation = encodeURIComponent(location);
+    var requestUrl = `${OPEN_WEATHER_MAP_URL}&q=${encodedLocation}`;
+    return axios.get(requestUrl).then(function(res){
+      if(res.data.cod && res.data.message){
+        throw new Error(res.data.message);
+      }else{
+        return res.data.main.temp;
+      }
+    },function(res){
+      throw new Error(res.data.message);
+    });
+  }
+}
---
+++ app/components/WeatherMessage.jsx
@@ -0,0 +1,17 @@
+var React = require('react');
+// var WeatherMessage = React.createClass({
+//   render: function(){
+//     var {temp, location} =this.props;
+//     return(
+//       <h3>Hace {temp} grados en {location}.</h3>
+//     );
+//   }
+// });
+
+var WeatherMessage = (props) => {
+  return(
+    <h3>Hace {props.temp} grados en {props.location}.</h3>
+  );
+};
+
+module.exports= WeatherMessage;
